1a30575429455deed7c78da35050445befac2deb
Kyle Taylor <55237295+kylextaylor@users.noreply.github.com>
2021-07-08 10:40:00 +0100

    Delete Remove Printers.ps1

--- Remove Printers.ps1
+++
@@ -1,9 +0,0 @@
-#Replace PRINTSERVER with name of the Print Server
-$PrintServer = "\\PRINTSERVER"
-
-$Printers = Get-WmiObject -Class Win32_Printer
-ForEach ($Printer in $Printers) {
-If ($Printer.SystemName -like "$PrintServer") {
-(New-Object -ComObject WScript.Network).RemovePrinterConnection($($Printer.Name))
-}
-}
